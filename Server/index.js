7e384c63094ca65bf8d290dcdf6760ddb2c22791
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-08-13 10:52:24 -0700

    created a express route

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,40 @@
-//Create a server
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('./db');
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/products', (req, res) => {
+  db.getProducts((err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
+
+})
+
+app.get('/reviews', (req, res) => {
+  db.getReviews((err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
+
+})
+
+app.listen(port, () => {
+  console.log (`App running on port ${port}.`);
+});
+
